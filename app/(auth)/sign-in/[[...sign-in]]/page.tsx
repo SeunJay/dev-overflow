91ce43fb6f91b70faf02c5754da7232d3abbe7ad
Oluwaseun Jayeoba <seunjay92@gmail.com>
2024-07-20 02:39:19 +0100

    create layout and set metadata at the root level

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -3,6 +3,7 @@ import { SignIn } from "@clerk/nextjs";
 
 const Signin = () => {
   return <SignIn />;
+  // <h1>Sign in</h1>;
 };
 
 export default Signin;
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -3,6 +3,7 @@ import { SignUp } from "@clerk/nextjs";
 
 const Signup = () => {
   return <SignUp />;
+  // <h1>Sign up</h1>;
 };
 
 export default Signup;
