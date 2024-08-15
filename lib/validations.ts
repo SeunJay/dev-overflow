b41a4fec5c6d41dc3f8f9a264bf50abc82515065
Oluwaseun Jayeoba <seunjay92@gmail.com>
2024-08-15 13:54:16 +0100

    complete styling of the ask a question main area

---
+++ lib/validations.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+export const QuestionsSchema = z.object({
+  title: z
+    .string()
+    .min(5, {
+      message: "Question must contain at least 5 characters",
+    })
+    .max(130),
+  explanation: z.string().min(100, {
+    message: "Explanation must contain at least 100 characters",
+  }),
+
+  tags: z.array(z.string().min(1).max(15)).min(1).max(3),
+});
